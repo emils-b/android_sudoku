360b9641247200468d21b094ad2addc2561884e6
Emils Baskers <emils.baskers@gmail.com>
2023-01-18 22:33:28 +0200

    next commit

--- app/src/main/java/lv/project/sudoku/GameActivity.java
+++ app/src/main/java/lv/project/sudoku/GameActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class GameActivity extends AppCompatActivity {
 
+    // TODO man nav telefona ar ko notestēt outlay priekš sudoku laukuma.
+    //  Izveidoju potenciālo variantu, bet nesaliku visus input fieldus, ja nu nestrādā tā.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/lv/project/sudoku/MainActivity.java
+++ app/src/main/java/lv/project/sudoku/MainActivity.java
@@ -3,8 +3,10 @@ package lv.project.sudoku;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,11 +16,15 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String DIFFICULTY_KEY = "difficulty_key";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+        
         Button btnNewGame = findViewById(R.id.newGameBtn);
         btnNewGame.setText(R.string.new_game_btn);
 
@@ -49,10 +55,13 @@ public class MainActivity extends AppCompatActivity {
                 Button positiveButton = dialog.getButton(AlertDialog.BUTTON_POSITIVE);
                 positiveButton.setOnClickListener(new View.OnClickListener() {
                     @Override
-                    public void onClick(View v) {
+                    public void onClick(View view) {
                         dialog.dismiss();
-                        int selectedPosition = ((AlertDialog) dialog).getListView().getCheckedItemPosition();
-                        //TODO selectedPosition jāsaglabā Shared preferences, lai spēles skatā varētu uzlikt
+                        //TODO atgriež indexu (int vērtība). Mož pēc indexa tad jāiegūst no strings.xml difficulty masīva teksta vērtība un tad ifā var noteikt grūtības pakāpi.
+                        int selectedDifficulty = dialog.getListView().getCheckedItemPosition();
+                        SharedPreferences.Editor editor = sharedPref.edit();
+                        editor.putInt(DIFFICULTY_KEY, selectedDifficulty);
+                        editor.apply();
 
                         Toast toast = Toast.makeText(MainActivity.this, R.string.new_game_started_toast, Toast.LENGTH_SHORT);
                         toast.show();
@@ -65,7 +74,9 @@ public class MainActivity extends AppCompatActivity {
         btnContinue.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //TODO izveidot iesākto spēļu ieguvi no Shared preferences
+                // TODO izveidot iesākto spēļu ieguvi no Shared preferences
+                // TODO izskatās, ka var glabāt tikai String vai int. Šajā gadījumā masīvs mirs glabāšanas jāpārveido par JSON
+                // TODO un pēc atgūsānas atpakaļ uz masīvu (https://stackoverflow.com/questions/3876680/is-it-possible-to-add-an-array-or-object-to-sharedpreferences-on-android)
                 String[] startedGames = {};
 
                 AlertDialog dialog = new AlertDialog.Builder(MainActivity.this)
